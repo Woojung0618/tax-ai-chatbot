31c9649cc2f8557205b2374ac42bfb50aed3cf31
전우정 <woojung0618@gmail.com>
2025-06-01 13:49:12 +0900

    pinecone 으로 변경

--- llm.py
+++ llm.py
@@ -1,19 +1,17 @@
-from langchain_openai import ChatOpenAI
-from langchain_openai import OpenAIEmbeddings
-from langchain_chroma import Chroma
-from langchain_openai import ChatOpenAI
-# from langchain import hub
-# from langchain.chains import RetrievalQA
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder, FewShotChatMessagePromptTemplate
-from langchain.chains import create_retrieval_chain
-from langchain.chains import create_history_aware_retriever
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
 
 from config import answer_examples
+
 store = {}
 
 
@@ -23,24 +21,17 @@ def get_session_history(session_id: str) -> BaseChatMessageHistory:
     return store[session_id]
 
 
-def get_llm(model='gpt-4o'):
-    llm = ChatOpenAI(model=model)
-    return llm
-
 def get_retriever():
     embedding = OpenAIEmbeddings(model='text-embedding-3-large')
-    database = Chroma(
-        collection_name='chroma-tax',
-        persist_directory='./chroma',
-        embedding_function=embedding
-    )
-    return database.as_retriever()
-
-llm = get_llm()
-retriever = get_retriever()
+    index_name = 'tax-index'
+    database = PineconeVectorStore.from_existing_index(index_name=index_name, embedding=embedding)
+    retriever = database.as_retriever(search_kwargs={'k': 4})
+    return retriever
 
 def get_history_retriever():
-
+    llm = get_llm()
+    retriever = get_retriever()
+    
     contextualize_q_system_prompt = (
         "Given a chat history and the latest user question "
         "which might reference context in the chat history, "
@@ -56,67 +47,57 @@ def get_history_retriever():
             ("human", "{input}"),
         ]
     )
-
+    
     history_aware_retriever = create_history_aware_retriever(
         llm, retriever, contextualize_q_prompt
     )
-
     return history_aware_retriever
+
+
+def get_llm(model='gpt-4o'):
+    llm = ChatOpenAI(model=model)
+    return llm
+
 
 def get_dictionary_chain():
     dictionary = ["사람을 나타내는 표현 -> 거주자"]
-
+    llm = get_llm()
     prompt = ChatPromptTemplate.from_template(f"""
         사용자의 질문을 보고, 우리의 사전을 참고해서 사용자의 질문을 변경해주세요.
-        만약 변경할 필요가 없다고 판단되면, 사용자의 질문을 변경하지 않아도 됩니다.
-        그런 경우에는 질문만 리턴해주세요.
+        만약 변경할 필요가 없다고 판단된다면, 사용자의 질문을 변경하지 않아도 됩니다.
+        그런 경우에는 질문만 리턴해주세요
         사전: {dictionary}
         
         질문: {{question}}
     """)
-    parser = StrOutputParser()
-    dictionary_chain = prompt | llm | parser
+
+    dictionary_chain = prompt | llm | StrOutputParser()
+    
     return dictionary_chain
 
-# def get_qa_chain():
-#     llm = get_llm()
-#     retriever = get_retriever()
-#     prompt = hub.pull("rlm/rag-prompt")
-
-#     qa_chain = RetrievalQA.from_chain_type(
-#         llm,
-#         retriever = retriever,
-#         chain_type_kwargs = {"prompt": prompt}
-#     )
-#     return qa_chain
 
 def get_rag_chain():
-
+    llm = get_llm()
     example_prompt = ChatPromptTemplate.from_messages(
         [
-            ('human', 'What is {input}?'), 
-            ('ai', '{answer}')
+            ("human", "{input}"),
+            ("ai", "{answer}"),
         ]
     )
-
     few_shot_prompt = FewShotChatMessagePromptTemplate(
-        examples=answer_examples,
         example_prompt=example_prompt,
+        examples=answer_examples,
     )
-
     system_prompt = (
-        # "You are an assistant for question-answering tasks. "
-        # "Use the following pieces of retrieved context to answer "
-        # "the question. If you don't know the answer, say that you "
-        # "don't know. Use three sentences maximum and keep the "
-        # "answer concise."
         "당신은 소득세법 전문가입니다. 사용자의 소득세법에 관한 질문에 답변해주세요"
-        "아래에 제공된 문서를 활용해서 답변해주시고 답변을 알 수 없다면 모른다고 답변해주세요"
-        "답변을 제공할 때는 소득세법 (xx조)에 따르면 이라고 시작하면서 답변해주세요"
-        "2-3문장 정도의 짧은 내용의 답변을 원합니다."
+        "아래에 제공된 문서를 활용해서 답변해주시고"
+        "답변을 알 수 없다면 모른다고 답변해주세요"
+        "답변을 제공할 때는 소득세법 (XX조)에 따르면 이라고 시작하면서 답변해주시고"
+        "2-3 문장정도의 짧은 내용의 답변을 원합니다"
         "\n\n"
         "{context}"
     )
+    
     qa_prompt = ChatPromptTemplate.from_messages(
         [
             ("system", system_prompt),
@@ -125,26 +106,25 @@ def get_rag_chain():
             ("human", "{input}"),
         ]
     )
-    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
     history_aware_retriever = get_history_retriever()
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
 
     rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
-
-
+    
     conversational_rag_chain = RunnableWithMessageHistory(
         rag_chain,
         get_session_history,
         input_messages_key="input",
         history_messages_key="chat_history",
         output_messages_key="answer",
-    ).pick("answer")
-
+    ).pick('answer')
+    
     return conversational_rag_chain
+
 
 def get_ai_response(user_message):
     dictionary_chain = get_dictionary_chain()
     rag_chain = get_rag_chain()
-
     tax_chain = {"input": dictionary_chain} | rag_chain
     ai_response = tax_chain.stream(
         {
