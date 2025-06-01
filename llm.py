eb2d3819ce85afb85093233f0293258cccbac03f
전우정 <woojung0618@gmail.com>
2025-06-01 15:21:45 +0900

    Change index name

--- llm.py
+++ llm.py
@@ -23,7 +23,7 @@ def get_session_history(session_id: str) -> BaseChatMessageHistory:
 
 def get_retriever():
     embedding = OpenAIEmbeddings(model='text-embedding-3-large')
-    index_name = 'tax-index'
+    index_name = 'tax-index-small'
     database = PineconeVectorStore.from_existing_index(index_name=index_name, embedding=embedding)
     retriever = database.as_retriever(search_kwargs={'k': 4})
     return retriever
